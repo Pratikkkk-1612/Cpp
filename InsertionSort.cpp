09e9ed5edf5cd22727ffc9c32fd099d3cb58a40c
Pratik <pratikpunwatkar16@gmail.com>
2026-03-29 23:23:51 +0530

    Fix insertion sort implementation

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,22 +3,24 @@ using namespace std;
 
 void Insertionsort(int arr[],int n){
     for(int i = 0;i<n;i++){
-        int j = 1;
-        while(j>0 && arr[j-1]>arr[j])
-        swap(arr[j],arr[j-1]);
-        j--;
+        int j = i;
+        while(j > 0 && arr[j-1]>arr[j]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
     }
 }
 
+
 int main(){
     int n;
-    cout<<"Enter the Size of the array"<<endl;
+    cout<<"Enter thes Size of the array"<<endl;
     cin>>n;
     int arr[n];
     cout<<"Enter the Elements of the array"<<endl;
     for(int i = 0; i<n;i++) cin>>arr[i];
     Insertionsort(arr,n);
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
+    for(int k = 0;k<n;k++){
+        cout<<arr[k]<<" ";
     }
 }
